150111d18f1298f29f6aa0ce57928f9ec9a4e958
Etrickery <ericspfister@gmail.com>
2018-03-29 20:40:27 -0400

    Initial Commit
    Setup java repository for learning to develop java applications

---
+++ Java_example_classes/src/java_example_classes/Java_example_classes.java
@@ -0,0 +1,21 @@
+/*
+ * Eric Pfister
+ * ericspfister@gmail.com
+ * 
+ */
+package java_example_classes;
+
+/**
+ *
+ * @author ericpfister
+ */
+public class Java_example_classes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
